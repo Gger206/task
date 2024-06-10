d9b889a5882f8948c31c624f03b4eb48fdb98bd1
Gger206 <arteml22012006@gmail.com>
2024-06-10 11:35:22 +0300

    program for extracting the square root is ready

--- library.c
+++ library.c
@@ -6,3 +6,17 @@ int main(int argc, char *argv[]) {
 if (argc != 2)
 return 1;
 
+char *input = argv[1];
+char *end;
+
+while (*input != '\0') {
+double num = strtod(input, &end);
+double sqrt_num = sqrt(num);
+
+printf("%.2f ", sqrt_num);
+
+input = end;
+}
+
+return 0;
+}
